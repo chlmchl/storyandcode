ddb51ee68810e9ce9df3483cec8430de6c193742
Chloe <ch.michel96@gmail.com>
2023-07-26 16:18:02 +0200

    update

--- flask_app.py
+++ flask_app.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template, request, jsonify, session, send_file
 from elevenlabs import voices, generate, stream, set_api_key
 import openai
 import os
+import random
 
 set_api_key("d33a219d3324143b9d803a9e4bec5480")
-openai.api_key = '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' 
+openai.api_key = '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' 
 
 app = Flask(__name__)
 # CORS(app)
@@ -21,6 +22,10 @@ intro = True
 b1 = False
 b2 = False
 b3 = False
+video_files_b1 = []
+video_files_b2 = []
+video_files_b3 = []
+video_files_x = []
 started = False
 
 # Function to get a list of all video filenames in the "batch_1" folder
@@ -52,6 +57,14 @@ def init_session_vars():
 
 @app.route('/')
 def index():   
+    global video_files_b1, video_files_b2, video_files_b3, video_files_x
+    video_files_b1 = get_batch_1_videos()
+    video_files_b2 = get_batch_2_videos()
+    video_files_b3 = get_batch_3_videos()
+    video_files_x = get_batch_x_videos()
+    print(video_files_b1)
+    print(video_files_b2)
+    print(video_files_b3)
     return render_template('index.html')
 
 @app.route ('/intro', methods=['POST', 'GET'])
@@ -65,6 +78,7 @@ def intro():
                     stream=True
                 )
         stream(audio_stream)
+
         intro = False
         b1 = True
     return jsonify({'response_text': response_text, 'is_final': False})
@@ -74,29 +88,30 @@ def video():
     if intro: 
         b_path = 'static/vids/batch_x/network_intro_dark_fast.mp4'
     elif b1:
-        video_files = get_batch_1_videos()
+        video_files = video_files_b1
         b_path = 'static/vids/batch_1/'
     elif b2:
-        video_files = get_batch_2_videos()
+        video_files = video_files_b2
         b_path = 'static/vids/batch_2/'
     elif b3:
-        video_files = get_batch_3_videos()
+        video_files = video_files_b3
         b_path = 'static/vids/batch_3/'
     
     if intro: 
         video_path = b_path
     else:
-        current_video_index = session.get('current_video_index', 0)
+        # current_video_index = session.get('current_video_index', 0)
 
-        if current_video_index >= len(video_files):
-            app.logger.error("Invalid video index: %d", current_video_index)
-            return jsonify({'video_url': '', 'is_final': True})
+        # if current_video_index >= len(video_files):
+            # app.logger.error("Invalid video index: %d", current_video_index)
+            # return jsonify({'video_url': '', 'is_final': True})
+        random_number = random.randint(0, len(video_files) - 1)
 
-        video_filename = video_files[current_video_index]
+        video_filename = video_files[random_number]
         video_path = b_path + video_filename
 
-        current_video_index = (current_video_index + 1) % len(video_files)
-        session['current_video_index'] = current_video_index
+        # current_video_index = (current_video_index + 1) % len(video_files)
+        # session['current_video_index'] = current_video_index
 
     return jsonify({'video_url': video_path, 'is_final': False})
 
@@ -155,32 +170,32 @@ def generate_audio():
 
     if b3:
         if(text_input != ""):
-            response = openai.ChatCompletion.create(
-                # engine='davinci:ft-personal:test-dv-2023-06-27-09-32-44',  # Choose the appropriate ChatGPT engine
-                model="gpt-3.5-turbo",
-                messages=[{"role": "system", "content":"You're a hypnotist trying rapid fire conversation where the user has to answer to a surrealist and abstract affirmations with the first things that comes to mind"}, 
-                          {"role": "assistant", "content":"You see a stream."}, 
-                          {"role": "user", "content":"I see a stream"},
-                          {"role": "assistant", "content":"Is the stream calm or turbulent?"}, 
-                          {"role": "user", "content":"Calm"},
-                          {"role": "assistant", "content":"What color is the water in the stream"}, 
-                          {"role": "user", "content":"The water is blue"},
-                          {"role": "assistant", "content":"there are fish swimming in the stream"}, 
-                          {"role": "user", "content":"Goldfish"}, 
-                          {"role": "assistant", "content":"Tranquility and serenity"}, 
-                          {"role": "user", "content":"Peace"},
-                          {"role": "assistant", "content":"A hidden treasure waiting to be discovered"}, 
-                          {"role": "user", "content":"Rich"}, 
-                          {"role": "assistant", "content":"Fairies, unicorns, and talking trees"}, 
-                          {"role": "user", "content":"Fairytale"}, 
-                          {"role": "assistant", "content":"A vial of sparkling water that grants eternal youth"}, 
-                          {"role": "user", "content":"Eternal"},
-                          {"role": "assistant", "content":"The stream is a portal"}, 
-                          {"role": "user", "content":"Ethereal"}],
+            response = openai.Completion.create(
+                engine='davinci:ft-personal:test-dv-2023-06-27-09-32-44',  # Choose the appropriate ChatGPT engine
+                # model="gpt-3.5-turbo",
+                # messages=[{"role": "system", "content":"You're a hypnotist trying rapid fire conversation where the user has to answer to a surrealist and abstract affirmations with the first things that comes to mind"}, 
+                #           {"role": "assistant", "content":"You see a stream."}, 
+                #           {"role": "user", "content":"I see a stream"},
+                #           {"role": "assistant", "content":"Is the stream calm or turbulent?"}, 
+                #           {"role": "user", "content":"Calm"},
+                #           {"role": "assistant", "content":"What color is the water in the stream"}, 
+                #           {"role": "user", "content":"The water is blue"},
+                #           {"role": "assistant", "content":"there are fish swimming in the stream"}, 
+                #           {"role": "user", "content":"Goldfish"}, 
+                #           {"role": "assistant", "content":"Tranquility and serenity"}, 
+                #           {"role": "user", "content":"Peace"},
+                #           {"role": "assistant", "content":"A hidden treasure waiting to be discovered"}, 
+                #           {"role": "user", "content":"Rich"}, 
+                #           {"role": "assistant", "content":"Fairies, unicorns, and talking trees"}, 
+                #           {"role": "user", "content":"Fairytale"}, 
+                #           {"role": "assistant", "content":"A vial of sparkling water that grants eternal youth"}, 
+                #           {"role": "user", "content":"Eternal"},
+                #           {"role": "assistant", "content":"The stream is a portal"}, 
+                #           {"role": "user", "content":"Ethereal"}],
                 prompt=text_input,
-                max_tokens=20,  # Adjust the response length as desired
+                max_tokens=30,  # Adjust the response length as desired
                 n=1,  # Generate a single response
-                stop=['.','?'],  # Optionally specify a stop token to end the response
+                stop=None,  # Optionally specify a stop token to end the response
                 temperature=0.7,  # Control the randomness of the response (0.0 to 1.0)
             )
 
